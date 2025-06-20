f3d2e4b7cd168d0b27c1b5cfdc2c4b8b0986cbe1
AArusshi82 <143922485+AArusshi82@users.noreply.github.com>
2025-06-20 15:35:12 +0530

    setup basic dotenv

--- FlightsAndSearchService/src/index.js
+++ FlightsAndSearchService/src/index.js
@@ -1,12 +1,11 @@
 const express=require("express");
-require('dotenv').config();
+const {PORT}=require("./config/serverConfig");
 
 const setupAndStartServer = async ()=>{
     const app=express();
-    const PORT=3000;
     app.listen(PORT,()=>{
         console.log(`Server started successfully at ${PORT}`);
-        console.log(process.env)
+        
     });
 }
 
