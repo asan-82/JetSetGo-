a92d693ffb68c35797b36890b863c5e212636e91
AArusshi82 <143922485+AArusshi82@users.noreply.github.com>
2025-06-21 17:09:05 +0530

    additonals

--- FlightsAndSearchService/src/index.js
+++ FlightsAndSearchService/src/index.js
@@ -1,11 +1,16 @@
 const express=require("express");
 const {PORT}=require("./config/serverConfig");
 
+
 const setupAndStartServer = async ()=>{
     const app=express();
+    
+    app.use(express.json());
+
     app.listen(PORT,()=>{
-        console.log(`Server started successfully at ${PORT}`);
         
+        console.log(`Server started successfully at ${PORT}`);
+     
     });
 }
 
