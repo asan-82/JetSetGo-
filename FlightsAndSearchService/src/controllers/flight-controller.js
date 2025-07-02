d9a6a94e9936637c746908834a5eb3ef3f3e0086
AArusshi82 <143922485+AArusshi82@users.noreply.github.com>
2025-07-02 17:39:21 +0530

    built custom filters for getting the flights

--- FlightsAndSearchService/src/controllers/flight-controller.js
+++ FlightsAndSearchService/src/controllers/flight-controller.js
@@ -23,6 +23,27 @@ const create = async (req, res) => {
   });
 }
 }
+const getAll = async (req, res) => {
+	try{
+        const response= await flightService.getAllFlights(req.query);
+        return res.status(200).json({
+      data: response,
+      success: true,
+      message: "Successfully fetched the flights",
+      err: {},
+    });
+    }
+    catch (error) {
+  console.log(error);
+  return res.status(error.statusCode || 500).json({
+    data: {},
+    success: false,
+    message: error.message || "Not able to fetch the flights",
+    err: error
+  });
+}
+}
 module.exports={
-create
+create,
+getAll
 }
--- FlightsAndSearchService/src/services/flight-service.js
+++ FlightsAndSearchService/src/services/flight-service.js
@@ -13,7 +13,23 @@ class FlightService{
                 throw {error: 'Arrival time cannot be less than the departure time'};
             }
            const airplane=await this.airplaneRepository.getAirplane(data.airplaneId);
+           if (!airplane) {
+    throw { error: 'No airplane found with the given ID' };
+}
            const flight=await this.flightRepository.createFlight({...data,totalSeats:airplane.capacity});
+           return flight;
+        }
+        catch(error)
+        {
+            console.log("Something went wrong in the service layer");
+             throw error;
+        }
+    }
+    async getAllFlights(data)
+    {
+        try{
+            
+           const flight=await this.flightRepository.getAllFlights(data);
            return flight;
         }
         catch(error)
@@ -24,4 +40,5 @@ class FlightService{
     }
 }
 
+    
 module.exports=FlightService;
